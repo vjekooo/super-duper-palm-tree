95d1e94f652d4fcd5edff3d2c69796d0d7acf07f
Vjeko <vjeko.pavicin@gmail.com>
2025-02-01 20:33:08 +0100

    Add tests for quote fetching success and error handling

--- src/hooks/useQuote.test.ts
+++ src/hooks/useQuote.test.ts
@@ -30,4 +30,49 @@ describe('useQuote Hook', () => {
 			consoleLogSpy.mockRestore()
 		})
 	})
+
+	it('handles error during quote fetching and calls console.log in OnReject', async () => {
+		const mockError = new Error('Network error')
+		mockedAxios.get.mockRejectedValueOnce(mockError)
+
+		const quotePromiseFn = () =>
+			mockedAxios.get('https://api.quotable.io/random').then(res => res.data)
+
+		const consoleLogSpy = jest.spyOn(console, 'log')
+
+		const { result } = renderHook(() => useQuote(quotePromiseFn))
+
+		await waitFor(() => {
+			expect(result.current.status).toBe('error')
+			expect(result.current.quoteData).toBeNull()
+			expect(result.current.error).toEqual(mockError.message)
+
+			expect(consoleLogSpy).toHaveBeenCalledWith(mockError)
+			consoleLogSpy.mockRestore()
+		})
+	})
+
+	it('handles successful quote fetching and changes status from pending to success', async () => {
+		const mockResponse = {
+			_id: 'Ghsgd',
+			content: 'Test quote.',
+			length: 45
+		}
+		mockedAxios.get.mockResolvedValueOnce({ data: mockResponse })
+
+		const quotePromiseFn = () =>
+			mockedAxios.get('https://api.quotable.io/random').then(res => res.data)
+
+		const { result } = renderHook(() => useQuote(quotePromiseFn))
+
+		expect(result.current.status).toBe('pending')
+		expect(result.current.quoteData).toBeNull()
+		expect(result.current.error).toBeNull()
+
+		await waitFor(() => {
+			expect(result.current.status).toBe('success')
+			expect(result.current.quoteData).toEqual(mockResponse)
+			expect(result.current.error).toBeNull()
+		})
+	})
 })
