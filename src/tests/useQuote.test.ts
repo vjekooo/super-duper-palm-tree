315bb9dc60cfea62c417387e45abfdb6813172cf
Vjeko <vjeko.pavicin@gmail.com>
2025-02-03 22:33:43 +0100

    Refactor useQuote tests and add reFetch functionality tests.

--- src/tests/useQuote.test.ts
+++ src/tests/useQuote.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, waitFor } from '@testing-library/react'
+import { act, renderHook, waitFor } from '@testing-library/react'
 import { useQuote } from '../hooks/useQuote'
 import axios from 'axios'
 
@@ -6,13 +6,13 @@ jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
 describe('useQuote Hook', () => {
-	it('handles successful quote fetching and calls console.log in onResolve', async () => {
-		const mockResponse = {
+	it('should successfully fetch a quote and call onResolve', async () => {
+		const mockQuote = {
 			_id: 'Ghsgd',
 			content: 'Test quote.',
 			length: 45
 		}
-		mockedAxios.get.mockResolvedValueOnce({ data: mockResponse })
+		mockedAxios.get.mockResolvedValueOnce({ data: mockQuote })
 
 		const quotePromiseFn = () =>
 			mockedAxios.get('https://api.quotable.io/random').then(res => res.data)
@@ -23,15 +23,15 @@ describe('useQuote Hook', () => {
 
 		await waitFor(() => {
 			expect(result.current.status).toBe('success')
-			expect(result.current.quoteData).toEqual(mockResponse)
+			expect(result.current.quoteData).toEqual(mockQuote)
 			expect(result.current.error).toBeNull()
 
-			expect(consoleLogSpy).toHaveBeenCalledWith(mockResponse)
+			expect(consoleLogSpy).toHaveBeenCalledWith(mockQuote)
 			consoleLogSpy.mockRestore()
 		})
 	})
 
-	it('handles error during quote fetching and calls console.log in OnReject', async () => {
+	it('should call OnReject when an error occurs', async () => {
 		const mockError = new Error('Network error')
 		mockedAxios.get.mockRejectedValueOnce(mockError)
 
@@ -52,13 +52,13 @@ describe('useQuote Hook', () => {
 		})
 	})
 
-	it('handles successful quote fetching and changes status from pending to success', async () => {
-		const mockResponse = {
+	it('should successfully fetch a quote and change status from pending to success', async () => {
+		const mockQuote = {
 			_id: 'Ghsgd',
 			content: 'Test quote.',
-			length: 45
+			length: 8
 		}
-		mockedAxios.get.mockResolvedValueOnce({ data: mockResponse })
+		mockedAxios.get.mockResolvedValueOnce({ data: mockQuote })
 
 		const quotePromiseFn = () =>
 			mockedAxios.get('https://api.quotable.io/random').then(res => res.data)
@@ -71,8 +71,46 @@ describe('useQuote Hook', () => {
 
 		await waitFor(() => {
 			expect(result.current.status).toBe('success')
-			expect(result.current.quoteData).toEqual(mockResponse)
+			expect(result.current.quoteData).toEqual(mockQuote)
 			expect(result.current.error).toBeNull()
 		})
+	})
+
+	it('should update quoteData when reFetch is called', async () => {
+		const mockQuotePromiseFn = jest.fn()
+
+		const mockQuote = {
+			_id: 'ApPgd',
+			content: 'Test quote for reFetch.',
+			length: 45
+		}
+		mockQuotePromiseFn.mockResolvedValueOnce(mockQuote)
+
+		const { result } = renderHook(() => useQuote(mockQuotePromiseFn))
+
+		expect(result.current.status).toBe('pending')
+		expect(result.current.quoteData).toBeNull()
+
+		await act(async () => {
+			await mockQuotePromiseFn
+		})
+
+		expect(result.current.status).toBe('success')
+		expect(result.current.quoteData).toEqual(mockQuote)
+
+		const newQuote = {
+			_id: 'LLigd',
+			content: 'New quote for reFetch.',
+			length: 20
+		}
+		mockQuotePromiseFn.mockResolvedValueOnce(newQuote)
+
+		await act(async () => {
+			result.current.reFetch()
+		})
+
+		expect(result.current.status).toBe('success')
+		expect(result.current.quoteData).toEqual(newQuote)
+		expect(mockQuotePromiseFn).toHaveBeenCalledTimes(2)
 	})
 })
