a42531d4bc18458ee204b5a9b4da33aae9915dc6
Vjeko <vjeko.pavicin@gmail.com>
2025-02-04 23:55:18 +0100

    Update test to check rendering of special characters

--- src/tests/MaskedQuoteText.test.tsx
+++ src/tests/MaskedQuoteText.test.tsx
@@ -4,7 +4,7 @@ import { MaskedQuoteText } from '../components/maskedQuoteText/MaskedQuoteText'
 
 describe('MaskedQuoteText', () => {
 	it('renders properly with the given props', () => {
-		const base = 'Test Quote'
+		const base = 'Test; Quote!'
 		const revealed = ['T', 'Q', 'o']
 
 		render(<MaskedQuoteText base={base} revealed={revealed} />)
@@ -12,6 +12,14 @@ describe('MaskedQuoteText', () => {
 		expect(screen.queryByText('T')).toBeInTheDocument()
 		expect(screen.queryByText('Q')).toBeInTheDocument()
 		expect(screen.queryByText('o')).toBeInTheDocument()
+		expect(screen.queryByText('!')).toBeInTheDocument()
+
+		const semicolonElement = screen.queryByText(';')
+		const nextElement = semicolonElement?.nextElementSibling
+
+		expect(nextElement).toBeTruthy()
+		expect(nextElement?.nodeName).toBe('DIV')
+		expect(nextElement).toHaveTextContent('')
 
 		expect(screen.queryByText('e')).not.toBeInTheDocument()
 		expect(screen.queryByText('s')).not.toBeInTheDocument()
